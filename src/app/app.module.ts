724df2de5b70af9747963704774070c259a3dca3
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 17:54:41 +0700

    mapping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in.component';
@@ -10,7 +11,9 @@ import { SignInComponent } from './components/sign-in.component';
     SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/sign-in.component.ts
+++ src/app/components/sign-in.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   template: `
         <h4>Sign In</h4>
-        <form class="form-group">
-            <input class="form-control" placeholder="Email" />
+        <form class="form-group" [formGroup]="formSignIn">
+            <input
+                class="form-control"
+                placeholder="Email"
+                formControlName="email"
+            />
             <br/>
-            <input class="form-control" placeholder="Password" />
+            <input
+                class="form-control"
+                placeholder="Password"
+                formControlName="password"
+                type="password"
+            />
             <br/>
             <button class="btn btn-success">Sign In</button>
         </form>
+        <pre>{{ formSignIn.value | json }}</pre>
   `,
   styles: [`form { width: 300px; }`]
 })
 
-export class SignInComponent {}
+export class SignInComponent {
+    formSignIn = new FormGroup({
+        email: new FormControl('teo@gmail.com'),
+        password: new FormControl(''),
+    });
+}
