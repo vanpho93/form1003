0e239582f842725ef122eeace3bd0c8e30751d0c
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-29 18:21:54 +0700

    disable

--- src/app/components/sign-in.component.ts
+++ src/app/components/sign-in.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   template: `
         <h4>Sign In</h4>
-        <form class="form-group" [formGroup]="formSignIn">
+        <form class="form-group" [formGroup]="formSignIn" (ngSubmit)="signIn();">
             <input
                 class="form-control"
                 placeholder="Email"
@@ -19,16 +19,27 @@ import { FormGroup, FormControl } from '@angular/forms';
                 type="password"
             />
             <br/>
-            <button class="btn btn-success">Sign In</button>
+            <button class="btn btn-success" [disabled]="formSignIn.invalid">
+                Sign In
+            </button>
         </form>
         <pre>{{ formSignIn.value | json }}</pre>
+        <pre>VALID: {{ formSignIn.valid }}</pre>
   `,
   styles: [`form { width: 300px; }`]
 })
 
 export class SignInComponent {
-    formSignIn = new FormGroup({
-        email: new FormControl('teo@gmail.com'),
-        password: new FormControl(''),
-    });
+    formSignIn: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.formSignIn = this.fb.group({
+            email: ['', [Validators.email, Validators.required]],
+            password: ['', [Validators.minLength(5), Validators.required]]
+        });
+    }
+
+    signIn() {
+        alert(JSON.stringify(this.formSignIn.value));
+    }
 }
